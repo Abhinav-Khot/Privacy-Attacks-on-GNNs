5cbdd32497aee8aa1a2bbcc0c0169ad570d31cde
Abhinav-Khot <23110006@iitgn.ac.in>
2025-03-14 19:33:35 +0530

    testing with pubmed

--- models/DPAR.py
+++ models/DPAR.py
@@ -11,8 +11,8 @@ class DPAR(nn.Module):
         super().__init__()
         self.W1 = params['W1:0']
         self.W2 = params['W2:0']
-        self.nclass = 7
-        self.nfeat = 2879
+        self.nclass = 3
+        self.nfeat = 500
         self.hidden_sizes = 32
         self.ReLU = nn.ReLU()
 
@@ -22,7 +22,7 @@ class DPAR(nn.Module):
             hidden_1 = torch.spmm(input, torch.Tensor(self.W1))
         else:
             hidden_1 = torch.mm(input, torch.Tensor(self.W1))
-        
+
         hidden_1 = self.ReLU(hidden_1)
 
         local_logits = torch.mm(hidden_1,torch.Tensor(self.W2))
@@ -31,11 +31,10 @@ class DPAR(nn.Module):
 
         alpha = 0.25
         deg_row = adj.sum(1).detach()
-        
+
         deg_row_inv_alpha = (1 - alpha) / np.maximum(deg_row, 1e-12)
         for i in range(2):
             logits = deg_row_inv_alpha[:, None] * (adj @ logits) + alpha * local_logits
 
         return F.log_softmax(logits, dim=1)
-        
-        
+
